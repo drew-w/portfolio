97d8443b727399abfa3df32c9a322490b28fa5a9
Drew <eesnamdoow@gmail.com>
2023-08-25 14:35:47 -0600

    fake data

--- src/hooks/wallets.ts
+++ src/hooks/wallets.ts
@@ -9,7 +9,7 @@ export const walletBalance: WalletBalance = {
     natg: 144115682140566568,
     debt: 26797813853987,
     bnb: 364489962752794376,
-    bgld: 4082326205866968447,
+    bgld: 408232620586696844,
     xplr: 286150295692796,
     bev: 1242085076428,
     dcm: 3161156652892563,
@@ -20,3 +20,25 @@ export const walletBalance: WalletBalance = {
     rev: 0
   }
 }
+
+// export const walletBalance: WalletBalance = {
+//   address: '0x4921de5ba3d015bf73623533170827b6dc14d3b3',
+//   balances: {
+//     usdt: 0,
+//     usdc: 0,
+//     alum: 0,
+//     grow: 0,
+//     natg: 144115682140566568,
+//     debt: 0,
+//     bnb: 0,
+//     bgld: 0,
+//     xplr: 0,
+//     bev: 0,
+//     dcm: 0,
+//     blox: 0,
+//     dlg: 0,
+//     btcb: 0,
+//     drip: 0,
+//     rev: 0
+//   }
+// }
