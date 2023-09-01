022186cbcb3c97540454a7a4da069199b5ff89dc
Billy <billy.iv.art@gmail.com>
2023-09-01 14:26:41 -0600

    Continued work on approve modal, stlying orientation etc.

--- src/components/Dashboard/ApproveModal.tsx
+++ src/components/Dashboard/ApproveModal.tsx
@@ -22,23 +22,38 @@ export function ApproveModal ({ isOpen, setIsOpen }: Props) {
   }
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal  isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent minW='500px' h='248px' style={{ top: '20px', left: '20px' }} borderRadius='12px'>
         <ModalHeader>Approve Wallet</ModalHeader>
 
-        <ModalBody>
-          <Text>
-            Collecting helps ensure that only legitimate data transactions are
-            added to a blockchain. Participants trying to earn a chance. Staking
-            helps ensure that only legitimate data transactions are added to a
-            blockchain. Participants trying to earn a chance.
-          </Text>
+        <ModalBody >
+          <Box
+            border='2px solid'
+            borderRadius='8px'
+            borderColor='box-border'
+            px='15px'
+            paddingTop='10px'
+            paddingBottom='16px'
+            boxShadow='box-shadow-primary'
+            w='440px'
+            h='94px'
+          >
+            <Text>
+              Collecting helps ensure that only legitimate data transactions are
+              added to a blockchain. Participants trying to earn a chance.
+              Staking helps ensure that only legitimate data transactions are
+              added to a blockchain. Participants trying to earn a chance.
+            </Text>
+          </Box>
         </ModalBody>
 
-        <ModalFooter marginTop='60px'>
+        <ModalFooter>
+          <Button padding='20px' variant='squareGray' onClick={onClose}>
+            Cancel
+          </Button>
           <Box flex='1' />
-          <Button variant='square' onClick={onClose}>
+          <Button padding='20px' variant='squareBlue' onClick={onClose}>
             Approve
           </Button>
         </ModalFooter>
