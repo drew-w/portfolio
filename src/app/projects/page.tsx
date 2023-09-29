9dcd89a5efdf2e73ee483853c574ffb2ae64c3e1
drew-w <eesnamdoow@gmail.com>
2023-09-29 10:36:09 -0600

    name change

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+//Chakra
+import { Flex, Grid, GridItem, Drawer, Skeleton } from '@chakra-ui/react'
+
+//Style UI
+import { BalanceCard } from '@components/BalanceCard'
+
+//Types
+
+export default function Projects () {
+  return (
+    <Flex as='main' w='full' justify='center' py='20px'>
+      {/* <BalanceCard /> */}
+      projects
+    </Flex>
+  )
+}
