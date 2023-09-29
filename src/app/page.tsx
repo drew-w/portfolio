f2adcb8c3f2d20221a067291d67ca900134bc236
drew-w <eesnamdoow@gmail.com>
2023-09-28 20:31:38 -0600

    added navigation using the nav bar at top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,15 @@
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import { Flex, Center } from '@chakra-ui/react'
-import logo from '@assets/debtlogo.svg'
-import Image from 'next/image'
 
 export function Page () {
   const router = useRouter()
   useEffect(() => {
-    // if (typeof window !== 'undefined')
     router.push('/home')
   })
   return (
     <Flex h='80vh' w='100vw' as='main'>
-      <Center w='full'>
-        <Image src={logo} height={70} alt='debt' />
-      </Center>
+      <Center w='full'></Center>
     </Flex>
   )
 }
--- src/theme/semanticTokens.ts
+++ src/theme/semanticTokens.ts
@@ -2,11 +2,24 @@ export const semanticTokens = {
   colors: {
     'bg-default': {
       default: '#FAFAFC',
-      _dark: '#121212'
+      _dark: '#1f1f1f'
     },
     'fg-default': {
       default: '#0E464B',
       _dark: '#1E1E1E'
+    },
+    'radio-bg': {
+      default: '#0E464B',
+      _dark: '#008b8b'
+    },
+    'radio-text': {
+      default: '#FFF',
+      _dark: '#0c0c0c'
+    },
+    'radio-border': {
+      // default: '#F28529',
+      default: '#F3F3F3',
+      _dark: '#4c4c4c'
     }
     // 'fg-default': {
     //   default: '#FFFFFF'
@@ -21,6 +34,9 @@ export const semanticTokens = {
     heading: 'inter'
   },
   shadows: {
-    'box-shadow-primary': '0px 1px 2px rgba(213,213,213, 0.5)'
+    'box-shadow-primary': {
+      default: '0px 1px 2px rgba(213,213,213, 0.5)',
+      _dark: 'none'
+    }
   }
 }
