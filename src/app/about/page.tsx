a8201c2335acf3c45e07c1792f7402472ab462c4
drew-w <eesnamdoow@gmail.com>
2023-09-29 22:04:58 -0600

    build errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,15 @@
 'use client'
 //Chakra
-import { Flex, Grid, GridItem, Drawer, Skeleton } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 
 //Style UI
-import { BalanceCard } from '@components/BalanceCard'
 
 //Types
 
 export default function About () {
   return (
     <Flex as='main' w='full' justify='center' py='20px'>
-      {/* <BalanceCard /> */}
-      about
+      coming soon
     </Flex>
   )
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,14 @@
 'use client'
 //Chakra
-import { Flex, Grid, GridItem, Drawer, Skeleton } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 
 //Style UI
-import { BalanceCard } from '@components/BalanceCard'
 
 //Types
 
 export default function Projects () {
   return (
     <Flex as='main' w='full' justify='center' py='20px'>
-      {/* <BalanceCard /> */}
       projects
     </Flex>
   )
